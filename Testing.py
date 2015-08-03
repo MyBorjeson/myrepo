d4fdc57154fb0ee55ef9c487db1c523b4ccf379e
My Boörjeson <myborjeson@gmail.com>
2015-08-03 14:17:52 +0200

    Testing file added
    Testing of integers

---
+++ Testing.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+x = 3 # an integer
+f = 3.1415926 # a floating real point
+name = "Python" # a string
+big = 358315791L # long, a very large number
+z = complex(2,3) #(s+3i) a complex number. Consists of real an imaginary part
+print (x)
+print (f)
+print (name)
+print (big)
+print (z)
